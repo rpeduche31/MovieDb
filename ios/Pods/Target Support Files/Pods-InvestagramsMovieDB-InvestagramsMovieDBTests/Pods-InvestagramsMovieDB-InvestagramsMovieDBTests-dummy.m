5349d375f4b3a073097eb39ffa3c16fa4b10d9e7
Chester Peduche <chester.peduche@whitecloak.com>
2021-04-08 14:22:47 +0800

    investagram Mobile App Technical Exam

---
+++ ios/Pods/Target Support Files/Pods-InvestagramsMovieDB-InvestagramsMovieDBTests/Pods-InvestagramsMovieDB-InvestagramsMovieDBTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InvestagramsMovieDB_InvestagramsMovieDBTests : NSObject
+@end
+@implementation PodsDummy_Pods_InvestagramsMovieDB_InvestagramsMovieDBTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-InvestagramsMovieDB/Pods-InvestagramsMovieDB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InvestagramsMovieDB : NSObject
+@end
+@implementation PodsDummy_Pods_InvestagramsMovieDB
+@end
